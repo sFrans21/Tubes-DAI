5d180ad69878878265c456e499018e54a1f72e12
MreffyH <118194238+MreffyH@users.noreply.github.com>
2024-11-10 06:50:46 +0700

    Add main.ipnyb, fix repo

--- src/algorithms/SteepestHillClimb.py
+++ src/algorithms/SteepestHillClimb.py
@@ -1,6 +1,6 @@
 # Steepest Hill Climbing Algorithm
 import time
-from magiccube import MagicCube
+from cube.Magiccube import MagicCube
 
 class SteepestHillClimbing:
     def __init__(self, max_iterations=1000):
@@ -44,13 +44,13 @@ class SteepestHillClimbing:
     def is_solved(self, score):
         return score == 0
 
-main = SteepestHillClimbing(max_iterations=100)
-print("Visualisasi kubus dari bawah ke atas, jadinya bayangkan tiap lapisan ditumpuk saja")
-print("Kubus awal (random):")
-print(main.initial_state)
-print("Skor awal:", main.objective_function)
+# main = SteepestHillClimbing(max_iterations=100)
+# print("Visualisasi kubus dari bawah ke atas, jadinya bayangkan tiap lapisan ditumpuk saja")
+# print("Kubus awal (random):")
+# print(main.initial_state)
+# print("Skor awal:", main.objective_function)
 
-final_state, final_score = main.search()
-print("Skor akhir:", final_score)
-print("Kubus akhir:")
-print(final_state)
+# final_state, final_score = main.search()
+# print("Skor akhir:", final_score)
+# print("Kubus akhir:")
+# print(final_state)
